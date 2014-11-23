857912de7fc2779441bab5ef94bb35891dd3fca8
M Woods <mkowoods@gmail.com>
2014-11-23 14:25:47 -0800

    updated html and rm.

--- jhds-pml-project/scratch-pad.R
+++ jhds-pml-project/scratch-pad.R
@@ -3,7 +3,7 @@ library(caret)
 
 set.seed(42)
 
-har <- read.csv("pml-training.csv")
+har <- read.csv("pml-training.csv", header = T)
 trainIdx <- createDataPartition(har$classe, p = 0.7, list = F)
 
 har.train <- har[trainIdx, ]
@@ -17,14 +17,78 @@ har.test.Y <- har.test[, 160]
 
 as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
 
-preProcess <- function(har.data){
-  #remove cases where new_window is set to yes
-  har.data <- subset(har.data, har.data$new_window == "no")
+convertFactorToNumeric <- function(df){
+  cols <- dim(df)[2]
+  for(i in 1:cols){
+    if(is.factor(df[, i])){
+      df[, i] <- as.numeric.factor(df[, i])
+    }
+  }
+  return(df)
+}
+
+colsWithLargeNumberNA <- function(df, NA.Threshold = 0.5){
+  cols <- dim(df)[2]
+  rows <- dim(df)[1]
+  
+  colsPastThreshold <- numeric()
+  
+  for(i in 1:cols){
+    proportionMissing <- sum(is.na(df[, i]))/rows
+    if(proportionMissing >= NA.Threshold){
+      #print(i)
+      colsPastThreshold <- c(colsPastThreshold, i)
+      #print(colsPastThreshold)
+    }
+  }
+  return(colsPastThreshold)
+}
+
+
+preProcessTrain <- function(X){
+  #remove non-numeric and other miscellaneous data points
   colsToRemove <- c("X", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", 
                     "cvtd_timestamp", "new_window", "num_window")
-  colRemoveBool <- names(har.data) %in% colsToRemove
-  har.data <- har.data[, !colRemoveBool]
-  har.data.nzv <- nearZeroVar(har.data)
-  har.data <- har.data[, -har.data.nzv]
-  return(har.data)
+  colRemoveBool <- names(X) %in% colsToRemove
+  X <- X[, !colRemoveBool]
+  
+  X <- convertFactorToNumeric(X)
+  
+  col.high.perc.NA <- colsWithLargeNumberNA(X)
+  
+  if(length(col.high.perc.NA) > 0){
+    X <- X[, -col.high.perc.NA]
+  }
+  
+  X.nzv <- nearZeroVar(X)
+  
+  if(length(X.nzv) > 0){
+    X <- X[, -X.nzv]
+  }
+  return(X)
 }
+
+har.train.X.preproc <- preProcessTrain(har.train.X)
+
+cols <- names(har.train.X.preproc)
+har.train.X.preproc$outcome <- har.train.Y
+
+
+test.pred <-  predict(modFit, har.test.X[, cols])
+sum(test.pred == har.test.Y)/length(har.test.Y)
+
+table(test.pred, har.test.Y)
+
+trControl = trainControl(method="cv", number = 4)
+
+modFit.dtree <- train(outcome~., data = har.train.X.preproc, method = "rpart")
+
+modFit.knn <- train(outcome~., data = har.train.X.preproc, method = "knn", preProcess = c("pca"))
+
+modFit.rf <- train(y = har.train.X.preproc$outcome, x = har.train.X.preproc[, -53], 
+                   method = "rf", 
+                   ntree = 150,
+                   #mtry = 27,
+                   trControl = trControl,
+                   do.trace = 25,
+                   tuneLength = 5)
