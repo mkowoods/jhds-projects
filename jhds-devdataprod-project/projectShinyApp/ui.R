ebe11856543e6fb8910483822a037f6eea946092
M Woods <mkowoods@gmail.com>
2014-11-24 00:31:24 -0800

    updates to description

--- jhds-devdataprod-project/projectShinyApp/ui.R
+++ jhds-devdataprod-project/projectShinyApp/ui.R
@@ -9,7 +9,11 @@ shinyUI(
     mainPanel(
       plotOutput(outputId = 'plotDepth'),
       tags$h3("Description"),
-      tags$p("The Purpose of this app is to explore how a geospatioal")
+      tags$p("The App is based on the quakes dataset and explores the effect
+             of using different values for the number of clusters on the 3 variables 
+             lat, long and depth(which can be adjusted using the slider). The size is the Mean Magnitude per Cluster.
+             What's interesting is to explore what level of information is retained as the number
+             of points decreases.")
     )
   )
 )
